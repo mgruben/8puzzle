9acebb061bf367a077eb3dee071d3c850ddff49d
mgruben <GrubenM@gmail.com>
2016-10-01 09:01:27 -0500

    Initialize Board API

---
+++ Board.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class Board {
+    public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
+                                           // (where blocks[i][j] = block in row i, column j)
+    public int dimension()                 // board dimension n
+    public int hamming()                   // number of blocks out of place
+    public int manhattan()                 // sum of Manhattan distances between blocks and goal
+    public boolean isGoal()                // is this board the goal board?
+    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
+    public boolean equals(Object y)        // does this board equal y?
+    public Iterable<Board> neighbors()     // all neighboring boards
+    public String toString()               // string representation of this board (in the output format specified below)
+
+    public static void main(String[] args) // unit tests (not graded)
+}
