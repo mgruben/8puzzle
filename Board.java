4e55f04024f583ff7c4cb80aa896bacd3c93796f
mgruben <GrubenM@gmail.com>
2016-10-01 09:05:19 -0500

    Create space for document comments and implementation

--- Board.java
+++ Board.java
@@ -20,16 +20,55 @@
  * @author Michael <GrubenM@GMail.com>
  */
 public class Board {
-    public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
-                                           // (where blocks[i][j] = block in row i, column j)
-    public int dimension()                 // board dimension n
-    public int hamming()                   // number of blocks out of place
-    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-    public boolean isGoal()                // is this board the goal board?
-    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y)        // does this board equal y?
-    public Iterable<Board> neighbors()     // all neighboring boards
-    public String toString()               // string representation of this board (in the output format specified below)
-
-    public static void main(String[] args) // unit tests (not graded)
+    private int n;
+    
+    /**
+     * construct a board from an n-by-n array of blocks
+     * (where blocks[i][j] = block in row i, column j).
+     * @param blocks 
+     */
+    public Board(int[][] blocks) {
+        
+    }
+    
+    // Board dimension n
+    public int dimension() {
+        return n;
+    }
+    
+    // Number of blocks out of place
+    public int hamming() {
+        
+    }
+    
+    // Sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        
+    } 
+    
+    // Is this board the goal board?
+    public boolean isGoal() {
+        
+    }
+    // A board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+        
+    }
+    // Does this board equal y?
+    public boolean equals(Object y) {
+        
+    }
+    
+    // All neighbording boards
+    public Iterable<Board> neighbors() {
+        
+    }
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        
+    }
+    // unit tests (not graded)
+    public static void main(String[] args) {
+        
+    }
 }
