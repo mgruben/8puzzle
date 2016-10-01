9f63577710b096664b247ba5195ddca5928a88e4
mgruben <GrubenM@gmail.com>
2016-10-01 09:32:49 -0500

    Initialize Solver API

---
+++ Solver.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2016 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class Solver {
+    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
+    public boolean isSolvable()            // is the initial board solvable?
+    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+    public static void main(String[] args) // solve a slider puzzle (given below)
+}
