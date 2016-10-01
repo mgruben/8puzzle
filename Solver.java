4d1897d737c255228b5f1dd1745cfe00689f34e2
mgruben <GrubenM@gmail.com>
2016-10-01 14:40:09 -0500

    Remove unused import

--- Solver.java
+++ Solver.java
@@ -3,7 +3,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
-import java.util.Iterator;
 
 /*
  * Copyright (C) 2016 Michael <GrubenM@GMail.com>
